fad42da0b50cf37a65a369cc02170a86bd48e85d
alitafreshi <alitafreshi123@yahoo.com>
2025-01-19 15:51:02 +0330

    add google play services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,9 @@ dependencies {
     //hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
+
+    //google play location services
+    implementation(libs.google.play.location.services)
 }
 
 kapt {
