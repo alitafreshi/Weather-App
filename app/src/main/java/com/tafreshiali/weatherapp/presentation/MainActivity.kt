80d820150564666b9d5b03b36c5dec2f3c18cb1c
alitafreshi <alitafreshi123@yahoo.com>
2025-01-22 19:20:30 +0330

    WIP Implementing app theme

--- app/src/main/java/com/tafreshiali/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/tafreshiali/weatherapp/presentation/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.tafreshiali.weatherapp.presentation.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,7 +25,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val colorStops = arrayOf(
+                    0.0f to Color(0xFFFEE3BC),
+                    1f to Color(0xFFF39876)
+                )
+                val brush = Brush.linearGradient(colorStops = colorStops)
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(brush)
+                ) { innerPadding ->
                     AppNavigation(modifier = Modifier.padding(innerPadding))
                 }
             }
