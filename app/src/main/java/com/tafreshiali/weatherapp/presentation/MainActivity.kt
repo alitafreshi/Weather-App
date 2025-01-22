69855d9334f593093fc01012dd1185608ba27531
alitafreshi <alitafreshi123@yahoo.com>
2025-01-22 19:38:29 +0330

    Implement app theme

--- app/src/main/java/com/tafreshiali/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/tafreshiali/weatherapp/presentation/MainActivity.kt
@@ -8,13 +8,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.tafreshiali.weatherapp.presentation.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,9 +29,10 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(brush)
                 ) { innerPadding ->
-                    AppNavigation(modifier = Modifier.padding(innerPadding))
+                    AppNavigation(modifier = Modifier
+                        .background(brush)
+                        .padding(innerPadding))
                 }
             }
         }
