68ff002aef51575cb723f39fba9a2d7f994f6431
alitafreshi <alitafreshi123@yahoo.com>
2025-01-26 20:14:42 +0330

    make status bar icons dark

--- app/src/main/java/com/tafreshiali/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/tafreshiali/weatherapp/presentation/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.tafreshiali.weatherapp.presentation
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowInsetsControllerCompat
 import com.tafreshiali.weatherapp.presentation.theme.design_system.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,18 +21,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val colorStops = arrayOf(
+                0.0f to WeatherAppTheme.colorScheme.primaryLight,
+                1f to WeatherAppTheme.colorScheme.primaryDark
+            )
+            val brush = Brush.linearGradient(colorStops = colorStops)
             WeatherAppTheme {
-                val colorStops = arrayOf(
-                    0.0f to WeatherAppTheme.colorScheme.primaryLight,
-                    1f to WeatherAppTheme.colorScheme.primaryDark
-                )
-                val brush = Brush.linearGradient(colorStops = colorStops)
-                AppNavigation(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(brush)
-                )
+                SetLightStatusBarIcons()
+                AppNavigation(modifier = Modifier
+                    .fillMaxSize()
+                    .background(brush))
             }
         }
     }
+}
+
+@Composable
+fun SetLightStatusBarIcons() {
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+    val windowInsetsController = WindowInsetsControllerCompat(window, view)
+    // Set the status bar icons to dark (black)
+    windowInsetsController.isAppearanceLightStatusBars = true
 }
